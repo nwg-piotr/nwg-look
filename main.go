46985b5efc8646664bdc13aefb2ebd5a066750d4
piotr <nwg.piotr@gmail.com>
2022-03-18 03:55:46 +0100

    experiments

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	// "log"
 
 	"github.com/gotk3/gotk3/gtk"
 )
@@ -9,33 +10,53 @@ import (
 func main() {
 	gtk.Init(nil)
 
-	gtkSettings, _ := gtk.SettingsGetDefault()
+	builder, _ := gtk.BuilderNewFromFile("/home/piotr/Code/nwg-look/glade/main.glade")
+	win, _ := getWindow(builder)
+	fmt.Println(win)
 
-	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
-	if err != nil {
-		log.Fatal("Unable to create window:", err)
-	}
+	// gtkSettings, _ := gtk.SettingsGetDefault()
+
+	// win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	// if err != nil {
+	// 	log.Fatal("Unable to create window:", err)
+	// }
 
 	win.Connect("destroy", func() {
 		gtk.MainQuit()
 	})
 
-	box, _ := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
-	win.Add(box)
+	// box, _ := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
+	// win.Add(box)
 
-	var names = [...]string{"Adwaita", "Adwaita-dark", "HighContrast", "Raleigh"}
+	// var names = [...]string{"Adwaita", "Adwaita-dark", "HighContrast", "Raleigh"}
 
-	for _, name := range names {
-		btn, _ := gtk.ButtonNew()
-		n := name
-		btn.SetLabel(name)
-		btn.Connect("clicked", func() {
-			gtkSettings.SetProperty("gtk-theme-name", n)
-		})
-		box.PackStart(btn, false, false, 10)
-	}
+	// for _, name := range names {
+	// 	btn, _ := gtk.ButtonNew()
+	// 	n := name
+	// 	btn.SetLabel(name)
+	// 	btn.Connect("clicked", func() {
+	// 		gtkSettings.SetProperty("gtk-theme-name", n)
+	// 	})
+	// 	box.PackStart(btn, false, false, 10)
+	// }
 
 	win.ShowAll()
 
 	gtk.Main()
+}
+
+// getWindow returns *gtk.Window object from the glade resource
+func getWindow(b *gtk.Builder) (*gtk.Window, error) {
+
+	obj, err := b.GetObject("window")
+	if err != nil {
+		return nil, err
+	}
+
+	window, ok := obj.(*gtk.Window)
+	if !ok {
+		return nil, err
+	}
+
+	return window, nil
 }
